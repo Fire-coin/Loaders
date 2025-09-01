5e448c21959bba6f35c54d8b42aac8e1c1038706
Fire Coin <maxim.m.kollar@gmail.com>
2025-09-01 16:51:09 +0200

    Now program prompts to enter a path, if nothing or . is entered, it defaults to current path the file is in. If the path is entered it will use that one, and if entered path is invalid, it will use the current path of the file.

--- tree.cpp
+++ tree.cpp
@@ -1,49 +1,45 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <filesystem>
-#include <algorithm> // sort
-#include <utility> // std::pair
-#include <deque>
+#include <iostream> // cout, cin, cerr, endl
+#include <string> // string
+#include <vector> // vector
+#include <filesystem> // path, directory_entry, directory_itrator
+#include <algorithm> // sort, replace
+#include <utility> // pair
+#include <deque> // deque
 
 using namespace std;
 
 namespace fs = filesystem;
 
-string getCurrentName(fs::path);
+string getCurrentName(const fs::path&);
 
 struct Directory
 {
     vector<string> dirs;
     vector<string> files;
     fs::path m_dirPath;
-
+    
     Directory(fs::path path) {
         m_dirPath = path;
         dirs.reserve(10);
         files.reserve(10);
-
-        for (const fs::directory_entry& entry : fs::directory_iterator(path)) {
-            if (entry.is_regular_file()) files.emplace_back(getCurrentName(entry.path()));
-            if (entry.is_directory()) dirs.emplace_back(getCurrentName(entry.path()));
+        
+        try {
+            for (const fs::directory_entry& entry : fs::directory_iterator(path)) {
+                if (entry.is_regular_file()) files.emplace_back(getCurrentName(entry.path()));
+                if (entry.is_directory()) dirs.emplace_back(getCurrentName(entry.path()));
+            }
+        } catch (const fs::filesystem_error& e) {
+            
         }
-
+        
         sort(dirs.begin(), dirs.end());
         sort(files.begin(), files.end());
     }
-
+    
     Directory() {
         m_dirPath = "";
     }
-
-    void displayAll() {
-        std::cout << "Directories:\n";
-        for (const auto& d : dirs) cout << d << endl;
-
-        std::cout << "\nFiles:\n";
-        for (const auto& f : files) cout << f << endl;
-    }
-
+    
     pair<string, Directory> getNext() {
         if (dirs.size() > 0) {
             string nextDir = dirs.back();
@@ -57,51 +53,63 @@ struct Directory
             return pair<string, Directory>("", Directory());
         }
     }
-
+    
     int getRemaining() const {
         return dirs.size() + files.size();
     }
 };
 
-
-void recursiveIteration(Directory);
+void recursiveIteration(Directory&);
 
 
 int main() {
-    Directory dir = Directory(fs::current_path());
-    recursiveIteration(dir);
+    string dir;
+    std::cout << "Enter directory: ";
+    getline(cin, dir);
+    if (dir.empty() || dir == ".") dir = fs::current_path().string();
+    replace(dir.begin(), dir.end(), '/', '\\');
+
+    fs::directory_entry entry = fs::directory_entry(fs::current_path() / dir);
+    if (!entry.exists()) {
+        cerr << "Invalid directory entered!\n";
+        cout << "Changing to current directory, press enter to proceed\n";
+        cin.get();
+        entry = fs::directory_entry(fs::current_path());
+    }
+    Directory curDir = Directory(entry);
+    recursiveIteration(curDir);
 
     return 0;
 }
 
-string getCurrentName(fs::path p) {
+
+string getCurrentName(const fs::path& p) {
     string str = p.string();
 
-    int index = str.find_last_of('/');
-    if (index == -1) index = str.find_last_of('\\');
+    int index = str.find_last_of('\\');
 
     return str.substr(index + 1, str.length() - index - 1);
 }
 
 
-void recursiveIteration(Directory startDirectory) {
+void recursiveIteration(Directory& startDirectory) {
     deque<Directory> dirStack;
 
     dirStack.push_back(startDirectory);
-
+    std::cout << getCurrentName(startDirectory.m_dirPath) << endl;
     while (dirStack.size() > 0) {
         Directory& curDir = dirStack.back();
         if (curDir.getRemaining() > 0) {
             auto[name, dir] = curDir.getNext();
             std::cout << "|";
             for (auto it = dirStack.begin(); it != dirStack.end() - 1; ++it) {
-                cout << "    ";
+                cout << "   ";
                 if ((it + 1)->m_dirPath == curDir.m_dirPath) cout << '|';
                 else cout << ((it + 1)->getRemaining() > 0 ? '|' : ' ');
             }
             if (!dir.m_dirPath.empty()) dirStack.push_back(dir);
 
-            cout << "----" << name << endl;
+            cout << "---" << name << endl;
 
         } else dirStack.pop_back();
     }
