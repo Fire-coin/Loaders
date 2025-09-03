4e5813be8029068cca9aaa776b3275ede85b2c01
Fire Coin <maxim.m.kollar@gmail.com>
2025-09-03 19:31:43 +0200

    Menu now scans for current directory and adds executables to the manu. Then it is possible to choose which one to execute and it will be executed.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,33 @@
 #include <iostream> // cout, endl
 #include <conio.h> // getch
-#include <string> 
-#include <vector> 
+#include <string>
+#include <vector>
 #include <stdlib.h> // system
+#include <filesystem> // path, directory_iterator
 
 using namespace std;
+namespace fs = filesystem;
 
 int runMenu(vector<string>&, int);
+int loadPrograms(vector<string>&);
+
+string getExtension(const string&);
+string getName(filesystem::path);
 
 int main() {
     vector<string> programs;
     
-    programs.push_back("tree.exe");
-    programs.push_back("hello.exe");
-    programs.push_back("prog.exe");
-
+    loadPrograms(programs);
     int val = 0;
     do {
         val = runMenu(programs, val);
-        
+        system("cls");
+        if (val != -1) {
+            system(programs[val].c_str());
+            cout << "Press enter to continue...\n";
+            cin.get();
+        }
+
     } while (val != -1);
     
     return 0;
@@ -32,7 +41,7 @@ int runMenu(vector<string>& menu, int current) {
         system("cls");
         for (int i = 0; i < menu.size(); ++i) {
             cout << (i == current ? "[]|" : "  |");
-            cout << menu[i] << endl;
+            cout << getName(menu[i]) << endl;
         }
         c = getch();
         if (c == 'j') {
@@ -45,4 +54,28 @@ int runMenu(vector<string>& menu, int current) {
     } while (c != 'q' && c != 13);
 
     return c == 'q' ? -1 : current;
+}
+
+string getName(filesystem::path path) {
+    string name = path.string();
+
+    int index = name.find_last_of('\\');
+    return name.substr(index + 1, name.size() - index - 1);
+}
+
+string getExtension(const string& filename) {
+    int index = filename.find_last_of('.');
+    return filename.substr(index + 1, filename.size() - index - 1);
+}
+
+int loadPrograms(vector<string>& programs) {
+
+    for (auto& f : fs::directory_iterator(fs::current_path())) {
+        if (f.is_regular_file()) {
+            string stringName = getName(fs::path(f));
+            if (getExtension(stringName) == "exe") 
+                programs.push_back(fs::path(f).string());
+        }
+    }
+    return 0;
 }
